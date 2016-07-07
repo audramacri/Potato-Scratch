85698002054061463c7cd6936dc42cf0a9ce78da
Zac Heisey <zheisey@gmail.com>
2016-07-06 20:34:55 -0400

    try to set up JS smoothscroll in script.js, but couldn't get it to work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-console.log('you can use ES6 here');
-
+/*
+Made some edits to this section of JS code from 6/5
+class, but Caniuse.com shows the scroll-behavior rule
+in CSS as only being supported in FF at the moment.
+*/
 var about = document.querySelector("a[href='#about']");
 var target = document.getElementById('about');
 
-
-aboutLink.addEventListener("click", function(event)){
+aboutLink.addEventListener("click", function(event) {
 	if (window.scrollTo) {
 		event.preventDefault();
-		window.scrollTo({"behavior": "smooth", "top": target.offsetTop});	
+		window.scrollTo({"behavior": "smooth", "top": target.offsetTop});
 	}
-	})
+});
+
+// Another attempt at smooth-scrolling Nav items in JS
+
+// About menu item smooth scroll
+function aboutScroll () {
+	var aboutNavScroll = document.getElementById('about');
+	aboutNavScroll.scrollTop = 50;
+};
